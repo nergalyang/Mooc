e8fe5ccb27be242e52754c8816f7ced72c32b2f6
nergalyang <NadesicoY@MacBook-Pro.local>
2015-09-18 15:37:50 -0400

    New lesson

--- DOM_Intermedium/slidingdoors/slidingdoors.js
+++ DOM_Intermedium/slidingdoors/slidingdoors.js
@@ -1,28 +1,28 @@
 window.onload = function() {
 
 	var box = document.getElementById('container');
-	var imgs = box.getElementsByTagName('img')
-	var imgsWidth = imgs[0].offsetWidth;
+	var xbb = box.getElementsByTagName('img')
+	var xbbWidth = xbb[0].offsetWidth;
 	var exposeWidth = 120;
-	var boxWidth = imgsWidth + (imgs.length-1)*exposeWidth;
+	var boxWidth = xbbWidth + (xbb.length-1)*exposeWidth;
 	box.style.width = boxWidth + 'px';
 
-	var translate = imgsWidth - exposeWidth;
+	var translate = xbbWidth - exposeWidth;
 
 	function setImage(){
-		for (var i = 1 ; i < imgs.length; i++) {
-		imgs[i].style.left =  imgsWidth + (i-1)*exposeWidth + 'px'
+		for (var i = 1 ; i < xbb.length; i++) {
+		xbb[i].style.left =  xbbWidth + (i-1)*exposeWidth + 'px'
 		};
 	};
 	setImage();
 
-	for (var i = 0; i < imgs.length; i++) {
+	for (var i = 0; i < xbb.length; i++) {
 
 		(function(i){
-			imgs[i].onmouseover = function(){
+			xbb[i].onmouseover = function(){
 			setImage();
 			for (var j = 1; j <= i; j++) {
-				imgs[j].style.left = parseInt(imgs[j].style.left,10) - translate + 'px';
+				xbb[j].style.left = parseInt(xbb[j].style.left,10) - translate + 'px';
 			};
 			};
 		})(i);
